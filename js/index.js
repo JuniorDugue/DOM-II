305c277bb89332c100bd14822d4d36bbfc7eba0f
Junior Dugue <jrdugue@gmail.com>
2019-05-05 22:40:49 -0400

    fired up css less, everything test well, added new edited header image i plan on animating via js with an event handler, added a scroll event to pop up when user reaches bottom of the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// scroll effect (pop up appears when you scroll to bottom of the page)
+window.addEventListener("scroll", ()=> {
+  // console.log("Scrolled!");
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+
+  // console.log(scrollable);
+
+  if(Math.ceil(scrolled) === scrollable) {
+    alert("You've reached the bottom!")
+  }
+});
+
+
+
+const popUp = document.querySelector("adventure");
